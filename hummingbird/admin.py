70bc03061f84ee9e0041f3a9d815aba5d2f34ab2
Ittai <ittai.barzilay@gmail.com>
2015-12-02 12:25:34 -0800

    more formatting of import order and newlines

--- hummingbird/admin.py
+++ hummingbird/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from hummingbird.models import UserProfile, UserDevice
 
+
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('name','last_played','song','length',)
 
-admin.site.register(UserProfile,UserProfileAdmin)
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(UserDevice)
--- hummingbird/forms.py
+++ hummingbird/forms.py
@@ -1,25 +1,33 @@
 from django import forms
-from models import UserProfile, UserDevice
 from django.contrib.auth.models import User
+from models import UserProfile, UserDevice
+
 
 class UserProfileForm(forms.ModelForm):
+
 	class Meta:
 		model = UserProfile
 		fields = ('name','song','length')
 
+
 class UserDeviceForm(forms.ModelForm):
+
 	class Meta:
 		model = UserDevice
 		fields = ('mac_id',)
 
+
 class UserSongForm(forms.ModelForm):
+
 	class Meta:
 		model = UserProfile
 		fields = ('song','length')
 
+
 class UserForm(forms.ModelForm):
+
 	password = forms.CharField(widget=forms.PasswordInput())
 
 	class Meta:
 		model = User
-		fields = ('username','email','password')
+		fields = ('username','email','password',)
