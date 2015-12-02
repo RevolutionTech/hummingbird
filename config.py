ebe27b70ac832073628dc8b4c99f198e63352a96
Ittai <ittai.barzilay@gmail.com>
2015-12-02 12:09:49 -0800

    converted config.py indents to spaces

--- config.py
+++ config.py
@@ -32,23 +32,23 @@ cache_time_seconds = 60
 
 # Network
 tcpdump_re_ignore = [
-	"^$",
-	"^(\d{2}:){2}\d{2}.\d+ \[\|802.11\]$",
-	"^(\d{2}:){2}\d{2}.\d+ \d+us tsft (short preamble )?\d+\.?\d? Mb/s \d+ MHz \(0x\d+\) -?\d+dB signal -?\d+dB noise antenna 0 \(H\) Unknown Ctrl SubtypeUnknown Ctrl Subtype$"
+    "^$",
+    "^(\d{2}:){2}\d{2}.\d+ \[\|802.11\]$",
+    "^(\d{2}:){2}\d{2}.\d+ \d+us tsft (short preamble )?\d+\.?\d? Mb/s \d+ MHz \(0x\d+\) -?\d+dB signal -?\d+dB noise antenna 0 \(H\) Unknown Ctrl SubtypeUnknown Ctrl Subtype$"
 ]
 tcpdump_re = {
-	# format: key (string) to tuple of regex (string) and list of groups to match (ints)
-	'regular_message': ("^(\d{2}:){2}\d{2}.\d+ (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*> (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*, .*$", [2, 4]),
-	'regular_broadcast': ("^(\d{2}:){2}\d{2}.\d+ (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*> Broadcast, .*$", [2]),
-	'message': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
-	'broadcast': ("^(\d{2}:){2}\d{2}.\d+ \d+.* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:Broadcast SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
-	'probe_request': ("^((\d{2}:){2}\d{2}.\d+ .* BSSID:Broadcast DA:Broadcast SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2})) .*$", [3]),
-	'probe_response': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
-	'acknowledgement': ("^(\d{2}:){2}\d{2}.\d+ .* RA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2]),
-	'data_iv_message': ("^(\d{2}:){2}\d{2}.\d+ .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
-	'data_iv_broadcast': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
-	'power_save_poll': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* TA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
-	'noise_antenna': ("^(\d{2}:){2}\d{2}.\d+ .* RA:Broadcast BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2])
+    # format: key (string) to tuple of regex (string) and list of groups to match (ints)
+    'regular_message': ("^(\d{2}:){2}\d{2}.\d+ (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*> (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*, .*$", [2, 4]),
+    'regular_broadcast': ("^(\d{2}:){2}\d{2}.\d+ (([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*> Broadcast, .*$", [2]),
+    'message': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
+    'broadcast': ("^(\d{2}:){2}\d{2}.\d+ \d+.* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:Broadcast SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
+    'probe_request': ("^((\d{2}:){2}\d{2}.\d+ .* BSSID:Broadcast DA:Broadcast SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2})) .*$", [3]),
+    'probe_response': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
+    'acknowledgement': ("^(\d{2}:){2}\d{2}.\d+ .* RA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2]),
+    'data_iv_message': ("^(\d{2}:){2}\d{2}.\d+ .* DA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4, 6]),
+    'data_iv_broadcast': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* SA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
+    'power_save_poll': ("^(\d{2}:){2}\d{2}.\d+ .* BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .* TA:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2, 4]),
+    'noise_antenna': ("^(\d{2}:){2}\d{2}.\d+ .* RA:Broadcast BSSID:(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}) .*$", [2])
 }
 
 # Debugging
