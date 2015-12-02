e5e355819e3c14a992c7fe12dd96c8e95ff112b4
Ittai <ittai.barzilay@gmail.com>
2015-12-02 13:58:50 -0800

    inline var declarations for hummingbird-ajax.js

--- hummingbird/static/js/hummingbird-ajax.js
+++ hummingbird/static/js/hummingbird-ajax.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
 
     $('.delete-user').hide();
 
-    $('.delete_device').click(function(){
-        var device;
-        var user;
-        device = $(this).attr("data-deviceid");
-        user = $(this).attr("data-userprofile");
+    $('.delete_device').click(function() {
+        var device = $(this).attr('data-deviceid');
+        var user = $(this).attr('data-userprofile');
         $.get('/hummingbird/delete_device/', {device_id: device, user_id: user}, function(data){
             $('#device-div').html(data)
         });
@@ -20,7 +18,7 @@ $(document).ready(function() {
         });
 
 
-    $('.delete-user').click(function(){
+    $('.delete-user').click(function() {
         var user;
         user = $(this).attr("data-userprofile");
         $.get('/hummingbird/delete_user/', {user_id: user}, function(data){
