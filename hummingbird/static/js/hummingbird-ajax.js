bbdb761e96820bcaef4666200c697f054a070283
Ittai <ittai.barzilay@gmail.com>
2015-12-02 12:31:21 -0800

    added additional indent to most of hummingbird-ajax.js

--- hummingbird/static/js/hummingbird-ajax.js
+++ hummingbird/static/js/hummingbird-ajax.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
 
-$('.delete-user').hide();
+    $('.delete-user').hide();
 
-$('.delete_device').click(function(){
-    var device;
-    var user;
-    device = $(this).attr("data-deviceid");
-    user = $(this).attr("data-userprofile");
-    $.get('/hummingbird/delete_device/', {device_id: device, user_id: user}, function(data){
-        $('#device-div').html(data)
+    $('.delete_device').click(function(){
+        var device;
+        var user;
+        device = $(this).attr("data-deviceid");
+        user = $(this).attr("data-userprofile");
+        $.get('/hummingbird/delete_device/', {device_id: device, user_id: user}, function(data){
+            $('#device-div').html(data)
+        });
+
     });
+        $(".user-div").hover( function() {
+                $(this).find('.delete-user').show();
+        },
+        function() {
+                $(this).find('.delete-user').hide();
+        });
 
-});
-    $(".user-div").hover( function() {
-            $(this).find('.delete-user').show();
-    },
-    function() {
-            $(this).find('.delete-user').hide();
+
+    $('.delete-user').click(function(){
+        var user;
+        user = $(this).attr("data-userprofile");
+        $.get('/hummingbird/delete_user/', {user_id: user}, function(data){
+        //    $('#users-div').html(data)
+        });
+        $(this).parent().parent().hide();
     });
-
-
-$('.delete-user').click(function(){
-    var user;
-    user = $(this).attr("data-userprofile");
-    $.get('/hummingbird/delete_user/', {user_id: user}, function(data){
-    //    $('#users-div').html(data)
-    });
-    $(this).parent().parent().hide();
-});
 
 
 });
