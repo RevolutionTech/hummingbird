7dfd1f23aaa7efe4b5310ef24f8ea2b57e405f8e
Ittai <ittai.barzilay@gmail.com>
2015-12-03 16:13:41 -0800

    removed comments from urls.py

--- hummingbird_django/urls.py
+++ hummingbird_django/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'hummingbird.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^hummingbird/',include('hummingbird.urls')),
     url(r'^hummingbird/', include('hummingbird.urls')),
 )
